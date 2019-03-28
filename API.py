881e91d48e3162645c47ebb9d83e9a9e903338ba
tw358 <893116358@qq.com>
2019-03-28 16:05:18 +0800

    12306

---
+++ API.py
@@ -0,0 +1,7 @@
+class API(object):
+    # 登录链接
+    login = 'https://kyfw.12306.cn/otn/resources/login.html'
+    # 验证码验证链接
+    captchaCheck = 'https://kyfw.12306.cn/passport/captcha/captcha-check'
+    # 获取验证码图片
+    captchaImage = 'https://kyfw.12306.cn/passport/captcha/captcha-image?login_site=E&module=login&rand=sjrand'
